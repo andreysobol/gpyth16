239a0d40e6ddc7de41662ac2d1088c5fe676e24c
unknown <oh@matterlabs.dev>
2022-04-10 18:37:36 +0100

    EC in progress

--- curve.py
+++ curve.py
@@ -2,3 +2,21 @@ b0_montgomery = 1677228023976091778849639189773160371881200845595694312256380166
 gminus1 = pow(2, 256 * (q-2), FieldQ.modulo)
 b0 = (gminus1 * b0_montgomery) % q
 b1 = 568440292453150825972223760836185707764922522371208948902804025364325400423
+
+from field import FieldQ12, FieldQ2, FieldQ
+
+class EllipticCurve:
+
+    def __init__(self, x_coord, y_coord) -> None:
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+
+class EllipticCurveG1(EllipticCurve):
+
+    @classmethod
+    def generator(cls):
+        gen = EllipticCurveG1(
+            
+        )
+
+        return gen
