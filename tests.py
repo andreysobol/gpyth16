45a8fdfb3dc35e951d85f3fe38770e3b80a6fcc1
unknown <oh@matterlabs.dev>
2022-04-10 18:12:46 +0100

    test for q12

--- tests.py
+++ tests.py
@@ -11,18 +11,22 @@ class UnitTest(unittest.TestCase):
         self.assertEqual(r.value[0].value, 1)
 
     def test_field_q12(self):
-        # f = FieldQ12([
-        #     FieldQ2([FieldQ(1), FieldQ(2)]),
-        #     FieldQ2([FieldQ(3), FieldQ(4)]),
-        #     FieldQ2([FieldQ(5), FieldQ(6)]),
-        #     FieldQ2([FieldQ(7), FieldQ(8)]),
-        #     FieldQ2([FieldQ(9), FieldQ(10)]),
-        #     FieldQ2([FieldQ(11), FieldQ(12)]),
-        # ])
-        f = FieldQ2([FieldQ(3), FieldQ(4)])
+        f = FieldQ12([
+            FieldQ2([FieldQ(1), FieldQ(2)]),
+            FieldQ2([FieldQ(3), FieldQ(4)]),
+            FieldQ2([FieldQ(5), FieldQ(6)]),
+            FieldQ2([FieldQ(7), FieldQ(8)]),
+            FieldQ2([FieldQ(9), FieldQ(10)]),
+            FieldQ2([FieldQ(11), FieldQ(12)]),
+        ])
         r = f.pow(FieldQ.modulus**12 - 1)
-        print(r.value[1].value)
-        self.assertEqual(True, True)
+
+        self.assertEqual(r.value[0].value[1].value, 0)
+        self.assertEqual(r.value[0].value[0].value, 1)
+        for i in range(1,6):
+            self.assertEqual(r.value[i].value[1].value, 0)
+            self.assertEqual(r.value[i].value[0].value, 1)
+
 
 
 if __name__ == '__main__':
