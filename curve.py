ebee7b036a981e98b4d6f407c99a37bac11c2213
unknown <oh@matterlabs.dev>
2022-04-10 18:45:20 +0100

    EC generator and b_coeff methods

--- curve.py
+++ curve.py
@@ -3,6 +3,11 @@ gminus1 = pow(2, 256 * (q-2), FieldQ.modulo)
 b0 = (gminus1 * b0_montgomery) % q
 b1 = 568440292453150825972223760836185707764922522371208948902804025364325400423
 
+x0 = 10857046999023057135944570762232829481370756359578518086990519993285655852781
+x1 = 11559732032986387107991004021392285783925812861821192530917403151452391805634
+y0 = 8495653923123431417604973247489272438418190587263600148770280649306958101930
+y1 = 4082367875863433681332203403145435568316851327593401208105741076214120093531
+
 from field import FieldQ12, FieldQ2, FieldQ
 
 class EllipticCurve:
@@ -16,7 +21,34 @@ class EllipticCurveG1(EllipticCurve):
     @classmethod
     def generator(cls):
         gen = EllipticCurveG1(
-            
+            FieldQ(1),
+            FieldQ(2)
         )
-
         return gen
+
+    @classmethod
+    def b_coeff(cls):
+        return FieldQ(3)
+
+class EllipticCurveG1(EllipticCurve):
+
+    @classmethod
+    def generator(cls):
+        gen = EllipticCurveG1(
+            FieldQ2([
+                FieldQ(x0),
+                FieldQ(x1)
+            ]),
+            FieldQ2([
+                FieldQ(y0),
+                FieldQ(y1)
+            ])
+        )
+        return gen
+
+    @classmethod
+    def b_coeff(cls):
+        return FieldQ2([
+                    FieldQ(b0),
+                    FieldQ(b1)
+                ])
